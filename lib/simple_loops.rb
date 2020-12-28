a5c61a3b33da291976cbaabebd89389abdd0ab61
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-28 18:10:12 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,8 +16,8 @@ end
 
 def output_array (array)
   count = 0
-  while count < array.length do
-    array[count].to_s
+ while count < array.length do
+    puts array[count]
     count += 1
   end
 end
