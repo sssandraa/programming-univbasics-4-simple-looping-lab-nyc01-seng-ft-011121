9040aa4ba16b0d11432a00d3496105faa24f9b42
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-28 18:13:53 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,7 +26,7 @@ def return_string_array(array)
   count = 0
   new_array = []
   while count < array.length do
-    array[count].to_s
+    new_array.psuh(array[count].to_s)
     count += 1
   end
 end
